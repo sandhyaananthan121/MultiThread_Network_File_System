5ac4da21d3b4b96167a8a95e64695adbae42d0c7
sandhyaananthan121 <sandhyaananthan@ufl.edu>
2024-04-18 18:55:23 -0400

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+    Socket requestSocket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+
+    void run(int port) {
+        try {
+            requestSocket = new Socket("localhost", port);
+            System.out.println("Connected to localhost on port " + port);
+            Scanner scanner = new Scanner(System.in);
+
+            // Initialize ObjectOutputStream once outside the loop
+            out = new ObjectOutputStream(requestSocket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(requestSocket.getInputStream());
+
+            String command;
+            do {
+                System.out.println("Write 'get' or 'upload' with file name or 'c' to quit:");
+                command = scanner.nextLine();
+
+                if (command.startsWith("get")) {
+                    // Send the get command to the server
+                    sendCommand(command);
+                    // Receive and process the file from the server
+                    receiveFile(command.split(" ")[1]);
+                    System.out.println("File received: " + command.split(" ")[1]);
+                } else if (command.startsWith("upload")) {
+                    // Send the upload command to the server
+                    sendCommand(command);
+                    // Upload the file to the server
+                    sendFile(command.split(" ")[1]);
+                    System.out.println("File uploaded: " + command.split(" ")[1]);
+                } else if (!command.equals("c")) {
+                    // Send other commands to the server
+                    sendCommand(command);
+                    // Process the response from the server
+                    processResponse();
+                }
+            } while (!command.equals("c"));
+        } catch (ConnectException e) {
+            System.err.println("Connection refused. You need to initiate a server first.");
+        } catch (UnknownHostException unknownHost) {
+            System.err.println("You are trying to connect to an unknown host!");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+                out.close();
+                requestSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void sendCommand(String command) {
+        try {
+            out.writeObject(command);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void receiveFile(String filename) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("new_" + filename)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void sendFile(String filename) {
+        try (FileInputStream fileInputStream = new FileInputStream(filename)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.flush();
+
+            // After sending the file, process the server's response
+            processResponse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    String processResponse() throws IOException, ClassNotFoundException {
+        return (String) in.readObject();
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        int port = Integer.parseInt(args[0]);
+        client.run(port);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        System.out.println("The server is running on port " + port);
+        try (ServerSocket listener = new ServerSocket(port)) {
+            int clientNum = 1;
+            while (true) {
+                Socket connection = listener.accept();
+                System.out.println("Client " + clientNum + " connected from " + connection.getInetAddress() + ":" + connection.getPort());
+                new Handler(connection, clientNum).start();
+                clientNum++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket connection;
+        private int clientNum;
+
+        public Handler(Socket connection, int clientNum) {
+            this.connection = connection;
+            this.clientNum = clientNum;
+        }
+
+        public void run() {
+            try (ObjectInputStream in = new ObjectInputStream(connection.getInputStream());
+                 ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream())) {
+
+                System.out.println("Handler for Client " + clientNum + " started.");
+
+                String command;
+                while (true) {
+                    command = (String) in.readObject();
+                    System.out.println("Received command from Client " + clientNum + ": " + command);
+                    if (command.equals("c")) {
+                        System.out.println("Client " + clientNum + " has ended the connection.");
+                        break;
+                    }
+                    if (command.startsWith("get")) {
+                        String filename = command.split(" ")[1];
+                        System.out.println("Client " + clientNum + " requested file: " + filename);
+                        sendFile(filename, out);
+                    } else if (command.startsWith("upload")) {
+                        String filename = command.split(" ")[1];
+                        System.out.println("Client " + clientNum + " uploading file: " + filename);
+                        receiveFile(filename, connection.getInputStream());
+                    } else {
+                        System.out.println("Unknown command from client " + clientNum);
+                    }
+                    out.writeObject("Command processed"); // Send response to client
+                    out.flush();
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                //e.printStackTrace();
+            } finally {
+                try {
+                    connection.close();
+                    System.out.println("Handler for Client " + clientNum + " closed.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void sendFile(String filename, ObjectOutputStream out) {
+            try (FileInputStream fileInputStream = new FileInputStream(filename)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void receiveFile(String filename, InputStream inputStream) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream("new_" + filename)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    fileOutputStream.write(buffer, 0, bytesRead);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
